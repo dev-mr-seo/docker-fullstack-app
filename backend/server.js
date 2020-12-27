d48b5fcbdacb995623d4b6dfc13446c3caaf8a30
afashs <dev.afashs@gmail.com>
2020-12-27 21:46:58 +0900

    first commit

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require('./db');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// 테이블 생성하기 
+db.pool.query(`create table lists(id integer AUTO_INCREMENT, value TEXT, PRIMARY KEY (id))`, (err, results, fileds) => {
+  console.log('results', results)
+})
+
+app.get('/api/values', function( req, res ) {
+  db.pool.query(`SELECT * FROM lists;`,(err, results, fields) => {
+    if (err)
+      return res.status(500).send(err);
+    else
+      return res.json(results);
+  });
+});
+
+app.post('/api/value', function( req, res, next ) {
+  db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}");`, 
+  (err, results, fields) => {
+    if(err)
+      return res.status(500).send(err);
+    else
+      return res.json({ success: true, value: req.body.value });
+  });
+});
+
+
+app.listen(5000, () => {
+  console.log('어플리케이션이 5000번 포트에서 시작되었습니다');
+});
+
